7caeff28998d5d5fb74ed1226b4e5232fdb61b23
Tuan Hoang <hatuanit1812@gmail.com>
2018-09-23 21:14:16 +0700

    commit

---
+++ DemoPetaPoco/Form1.cs
@@ -0,0 +1,33 @@
+﻿using DemoPetaPoco.Mapping;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoPetaPoco
+{
+    public partial class Form1 : Form
+    {
+        private PetaPoco.Database dbContext = new PetaPoco.Database("DatabaseContext"); // connectionStrings name
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var list = dbContext.Query<M01_Item>(string.Empty);
+
+            var list2 = dbContext.Query<M01_Item>($@"select top 3 * from M01_Item");
+
+
+
+
+        }
+    }
+}
---
+++ DemoPetaPoco/Mapping/M01_Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPetaPoco.Mapping
+{
+    [PetaPoco.PrimaryKey("Id", AutoIncrement =true)]
+    public partial class M01_Item
+    {
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string UnitName { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
